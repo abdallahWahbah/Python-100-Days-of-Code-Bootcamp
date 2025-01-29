3414626d2ea10bf3c326c98d56b6302acbb4fd9e
Abdallah Wahbah <abdowahbah@gmail.com>
2025-01-29 16:16:28 +0200

    print-input-variables

---
+++ 1-print-input-variables/main.py
@@ -0,0 +1,13 @@
+print("Hello world!\nHello world!")
+print("Hello, " + "Abdallah")
+# This is a comment
+print('your age is: ' + input('What is your age? '))
+
+
+name = input('What is your name? ')
+print('Your name is: ' + name)
+print('Length of you name is', len(name))
+# print('Family name length is: ', len(input('What is your family name to print its length? ')))
+familyName = input('What is your family name to print its length? ')
+familyNameLength = len(familyName)
+print('Family name length is: ', familyNameLength)
